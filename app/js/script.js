fcd931b62bdea86b9d04d8b16738440cff331215
wild-dino <daritie8@gmail.com>
2022-02-26 12:23:30 +0300

    adding my files to my repository

---
+++ app/js/script.js
@@ -0,0 +1,24 @@
+const btnHumburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHumburger.addEventListener("click", function () {
+  console.log("open humburger");
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll")
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    body.classList.add('noscroll')
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
